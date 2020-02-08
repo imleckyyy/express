7c82fd5c317b7010a3847065814c3d0965c425c9
Patryk Pilecki <patrykpilecki@pc234.home>
2020-02-08 17:38:37 +0100

    serverjs cut

--- server.js
+++ server.js
@@ -6,18 +6,4 @@ const port = process.env.SV_PORT;
 
 app.get('/api/', (req, res) => res.send('Hello World!'));
 
-app.get('/api/user', function (req, res) {
-    res.send('Got a PUT request at /user');
-});
-
-app.get('/', (req, res) => res.json({
-    error: false,
-    errorText: 'brak bledow xd',
-}));
-
-app.get('/api/users', function (req, res) {
-    console.log(req.query.test);
-    res.send('got it');
-});
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
