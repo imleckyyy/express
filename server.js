9ea517783bd106596e3b615e7f7608b01d0a8034
imleckyyy <pileckipatryk@outlook.com>
2020-02-12 16:39:32 +0100

    adding babel and sequalize

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-require('dotenv').config();
+import 'dotenv/config';
 
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = process.env.SV_PORT;
 
-app.get('/api/', (req, res) => res.send('Hello World!'));
+app.get('/api/', function(req, res) {
+    return res.json({
+        text: 'Hello World!',
+    });
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
